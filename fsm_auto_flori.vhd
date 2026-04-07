68c4e92eb21211e9b6eadd82b7b974b7569d404c
Cretu Maria <cretumadalina70@gmail.com>
2026-04-07 17:18:39 +0300

    Add files via upload

---
+++ fsm_auto_flori.vhd
@@ -0,0 +1,5 @@
+<?xml version="1.0"?>
+<Runs Version="1" Minor="0">
+	<Run Id="synth_1" LaunchDir="C:/Users/adina/OneDrive - Technical University of Cluj-Napoca/Desktop/proiect_scid_automat_flori/proiect_scid_automat_flori/proiect_scid_automat_flori.runs/synth_1" FlowId="Vivado_Synthesis" FromStepId="vivado" ToStepId="vivado"/>
+</Runs>
+
